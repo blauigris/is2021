0b6644576d0b3659279b76468746392242740b19
Carles <carles@iMac-de-Carla.local>
2021-04-28 12:56:03 +0200

    Add additional visualization routines

--- Assignment 4/visualization.js
+++ Assignment 4/visualization.js
@@ -24,3 +24,30 @@ window.addEventListener('paste', function(event){
     console.log(cell.get_text());
 
 });
+
+window.addEventListener('copy', function(event){
+    event.preventDefault();
+    var new_data = event.clipboardData.getData('Text');
+    var cell = IPython.notebook.get_cell(5)
+    var raw = cell.get_text();
+    var data_div = $.parseHTML(raw)[0];
+    var content_div = data_div.firstChild;
+    var raw_data = content_div.textContent;
+
+    var data = JSON.parse(raw_data);
+    
+    var timestamp = Date.now();
+    var timeFormatter = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' });
+    var formattedTimestamp = timeFormatter.format(timestamp);
+    
+    data[formattedTimestamp] = new_data;
+    raw_data = JSON.stringify(data);
+    
+    content_div.textContent = raw_data
+    cell.set_text(data_div.outerHTML)
+        cell.render()
+
+    
+    console.log(cell.get_text());
+
+});
